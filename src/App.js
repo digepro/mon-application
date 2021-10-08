823bc9d84d4d73bf96abe6dcaf6b24a792e35a42
digepro <68436002+digepro@users.noreply.github.com>
2021-10-08 12:35:45 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import VerticalTabs from './Welcome'
+import Login from './Login'
+function App() {
+  return (
+    <div className="App">
+     <VerticalTabs/>
+    </div>
+  );
+}
+
+export default App;
